a708764e1e0edc91bb28322a8cf344ec27d468d8
Martin Mwaka <martinmwaka@hotmail.com>
2026-08-03 19:37:38 +0100

    Update script.js to render all films

--- script.js
+++ script.js
@@ -7,17 +7,52 @@
  5. Show source of data
 
  */
-const filmCardTemplate = document
-  .getElementById('film-card')
-  .content.cloneNode(true);
+
+const filmGrid = document.getElementById('film-grid');
 const allEpisodes = getAllEpisodes();
+
+const formatEpisodeCode = (prefix, value) => {
+  return `${prefix}${String(value).padStart(2, 0)}`;
+};
+
+const createFilmCard = ({
+  name,
+  number,
+  season,
+  summary,
+  image: { medium },
+}) => {
+  const filmCardTemplate = document
+    .getElementById('film-card')
+    .content.cloneNode(true);
+  const filmTitle = filmCardTemplate.querySelector('h2');
+  filmTitle.innerText = `${name} - ${formatEpisodeCode('S', season)}${formatEpisodeCode('E', number)}`;
+
+  const filmImage = filmCardTemplate.querySelector('img');
+  filmImage.src = medium;
+  filmImage.alt = 'image from film';
+
+  const filmSummary = filmCardTemplate.querySelector('.summary');
+  filmSummary.innerHTML = summary;
+
+  return filmCardTemplate;
+};
+
 function setup() {
-  makePageForEpisodes(allEpisodes);
+  // makePageForEpisodes(allEpisodes);
+  render();
 }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById('root');
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+const render = () => {
+  const filmCards = allEpisodes.map(createFilmCard);
+  console.log(filmCards);
+  filmGrid.innerHTML = '';
+  filmGrid.append(...filmCards);
+};
+
+// function makePageForEpisodes(episodeList) {
+//   const rootElem = document.getElementById('root');
+//   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+// }
 
 window.onload = setup;
