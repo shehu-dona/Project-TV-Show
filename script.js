8e399d54fafb07cc644ac3ef7cd0587574c41ea5
Martin Mwaka <martinmwaka@hotmail.com>
2026-08-03 11:17:41 +0100

    set up template card in html and add css for the webpage

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 //You can edit ALL of the code here
+/**
+ 1. Fetch all episodes
+ 2. Filter episodes - show numbers filtered
+ 3. Add css
+ 4. Clone repository
+ 5. Show source of data
+
+ */
+const filmCardTemplate = document
+  .getElementById('film-card')
+  .content.cloneNode(true);
+const allEpisodes = getAllEpisodes();
 function setup() {
-  const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
+  const rootElem = document.getElementById('root');
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 }
 
